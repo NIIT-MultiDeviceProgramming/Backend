c716cd93fc69767f98c9f5660c7be1e946f39e17
Aditya-jain23 <kumaradityaj.aj@gmail.com>
2022-02-26 23:50:06 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const req = require('express/lib/request');
+const { json } = require('express/lib/response');
+const app = express();
+const morgan = require('morgan');
+const mongoose =require('mongoose');
+const { stringify } = require('nodemon/lib/utils');
+const cors = require('cors');
+require('dotenv/config');
+
+
+app.use(cors());
+app.options('*',cors());
+
+
+//middleware
+app.use(express.json());
+app.use(morgan('tiny'));
+
+//routes
+const productsRoutes = require('./routes/products') ;
+const categoriesRoutes = require('./routes/categories');
+const ordersRoutes = require('./routes/orders');
+const productsRoute = require('./routes/products');
+const usersRoutes = require('./routes/users')
+
+const api = process.env.API_URL;
+
+app.use(`${api}/products`,productsRoutes);
+app.use(`${api}/categories`,categoriesRoutes);
+app.use(`${api}/orders`,ordersRoutes);
+app.use(`${api}/users`,usersRoutes);
+
+//database
+mongoose.connect(process.env.CONNECTION_STRING)
+.then(()=>{
+    console.log('MongoDB Database up and running!...')
+})
+.catch((err)=>{
+    console.log(err)
+})
+
+//server
+app.listen(3000, ()=>{
+    console.log(api)
+    console.log('Server running on http://127.0.0.1:3000');
+})
