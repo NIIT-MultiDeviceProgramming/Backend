6b763aa49cffc9aa71513a8cf8ba4ba20ee16108
Aditya-jain23 <kumaradityaj.aj@gmail.com>
2022-03-01 00:21:15 +0530

    trial

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 
 router.get(`/`, async (req, res) =>{
-    const userList = await User.find();
+    const userList = await User.find().select('-passwordHash');
 
     if(!userList) {
         res.status(500).json({success: false})
@@ -12,12 +12,22 @@ router.get(`/`, async (req, res) =>{
     res.send(userList);
 })
 
+
+router.get('/:id', async (req, res) =>{
+    const user = await User.findById(req.params.id).select('-passwordHash');
+
+    if(!user){
+        res.status(500).json({message: 'User not found'})
+    }
+    res.status(200).send(user);
+})
+
 router.post('/', async (req, res) =>{
     let user = new User({
         name: req.body.name,
         email: req.body.email,
         color: req.body.color,
-        passwordHash: bcrypt.hashSync(req.body.password, 10),
+        passwordHash:bcrypt.hashSync(req.body.password, 10),
         phone: req.body.phone,
         isAdmin: req.body.isAdmin,
         apartment: req.body.apartment,
@@ -26,6 +36,31 @@ router.post('/', async (req, res) =>{
         country: req.body.country,
     })
     user = await user.save();
+
+    if(!user)
+    return res.status(400).send('The user cannot be created')
+
+    res.send(user);
+})
+
+
+router.put('/:id', async (req, res)=> {
+    const user = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            email: req.body.email,
+            color: req.body.color,
+            passwordHash:bcrypt.hashSync(req.body.password, 10),
+            phone: req.body.phone,
+            isAdmin: req.body.isAdmin,
+            apartment: req.body.apartment,
+            zip: req.body.zip,
+            city: req.body.city,
+            country: req.body.country,
+        },
+        { new: true}
+    )
 
     if(!user)
     return res.status(400).send('The user cannot be created')
