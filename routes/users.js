d3aef52f15f743b11208622749143519877b2b95
agamgoel071 <75209765+agamgoel071@users.noreply.github.com>
2022-03-01 00:00:50 +0530

    comments

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 
+//To get the ouput list of categories
 router.get(`/`, async (req, res) =>{
     const userList = await User.find();
 
@@ -12,12 +13,13 @@ router.get(`/`, async (req, res) =>{
     res.send(userList);
 })
 
+//To get all the input to creat new categories
 router.post('/', async (req, res) =>{
     let user = new User({
         name: req.body.name,
         email: req.body.email,
         color: req.body.color,
-        passwordHash: bcrypt.hashSync(req.body.password, 10),
+        passwordHash: bcrypt.hashSync(req.body.password, 10),//We have hashed the passowrd incase our database becomes vulnerable and omeone without athourity would be able to see our data
         phone: req.body.phone,
         isAdmin: req.body.isAdmin,
         apartment: req.body.apartment,
